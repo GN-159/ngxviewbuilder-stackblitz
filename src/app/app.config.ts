83c847b86c0a17e9580ad57769cbeff9fe294081
GN-159 <ngxvbapp@gmail.com>
2026-07-25 22:14:17 +0300

    added template tab and provideZoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig,  provideZonelessChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideNgxViewBuilderRuntime } from 'ngx-view-builder';
 import { routes } from './app.routes';
+import { provideNgxViewBuilderTemplates } from 'ngx-view-builder-plugin-templates';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
+    provideZonelessChangeDetection(),
     provideNgxViewBuilderRuntime({ preloadRuntimeServices: true }),
+    provideNgxViewBuilderTemplates(),
   ],
 };
